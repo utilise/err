1b939cfe358c354cd3a8cba2025b75422c3c3713
pemrouz <p3pedram@hotmail.com>
2015-05-04 17:14:11 +0100

    multiple args and color

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-module.exports = function err(prefix){
+module.exports = function log(prefix){
   return function(d){
-    return console.error(prefix, d), d
+    return console.error.bind(console, String.grey ? prefix.grey : prefix).apply(console, arguments), d
   }
 }
