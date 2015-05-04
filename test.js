06500de5d955a6d30032c6af933704ae72011296
pemrouz <p3pedram@hotmail.com>
2015-05-04 15:54:07 +0100

    update name

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var expect = require('chai').expect
   , err = require('./')('sth')
 
-describe('error', function() {
+describe('err', function() {
   it('should error value untouched and return it', function() {
     var o = { a: 1 }
     expect(err(o)).to.equal(o)
