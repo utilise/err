3c35ce6280f4345848a5b996922e55e514e403c4
pemrouz <p3pedram@hotmail.com>
2015-05-14 00:58:49 +0100

    use correct console api

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ module.exports = function err(prefix){
   return function(d){
     var args = [].slice.call(arguments, 0)
     args.unshift(''.red ? prefix.red : prefix)
-    return console.log.apply(console, args), d
+    return console.error.apply(console, args), d
   }
 }
