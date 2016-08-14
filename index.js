2df7c035c0b70b4054094541bf810f1338c5b14f
pemrouz <p3pedram@hotmail.com>
2016-08-14 21:11:26 +0100

    feat: add timestamps

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-var owner = require('utilise.owner')
+var is = require('utilise.is')
   , to = require('utilise.to')
+  , owner = require('utilise.owner')
 
-module.exports = function err(prefix){
+module.exports = function err(ns){
   return function(d){
     if (!owner.console || !console.error.apply) return d;
+    is.arr(arguments[2]) && (arguments[2] = arguments[2].length)
     var args = to.arr(arguments)
+      , prefix = '[err][' + (new Date()).toISOString() + ']' + ns
+
     args.unshift(prefix.red ? prefix.red : prefix)
     return console.error.apply(console, args), d
   }
--- test.js
+++ test.js
@@ -1,41 +1,70 @@
 var expect = require('chai').expect
   , client = require('utilise.client')
-  , owner = require('utilise.owner')
-  , err = require('./')
-  , to = require('utilise.to')
+  , owner  = require('utilise.owner')
+  , to     = require('utilise.to')
+  , err    = require('./')
+  , iso    = '\\d{4}-[01]\\d-[0-3]\\dT[0-2]\\d:[0-5]\\d:[0-5]\\d\\.\\d+([+-][0-2]\\d:[0-5]\\d|Z)'
+  , output, realLog, realConsole
 
 describe('err', function() {
-  it('should error value untouched and return it', function() {
-    var o = { a: 1 }
-    expect(err('foo')(o)).to.equal(o)
+
+  it('should do nothing if no console.error', function() {
+    init(function(){
+      delete owner.console
+      expect(err('[foo]')('bar')).to.equal('bar')
+      expect(output).to.not.be.ok
+    })
   })
 
-  it('should do nothing if no console', function() {
-    var realConsole = owner.console
-    delete owner.console
-    expect(err('foo')('bar')).to.equal('bar')
-    owner.console = realConsole
+  it('should err value untouched and return it', function() {
+    init(function(){
+      var o = { a: 1 }
+      expect(err('[foo]')(o)).to.equal(o)
+      expect(reg('foo').test(output)).to.be.ok
+    })
+  })
+
+  it('should loop over arrays compactly', function() {
+    init(function(){
+      ['a','b','c'].map(err('[foo]'))
+      expect(reg('foo').test(output)).to.be.ok
+      expect(reg('foo', ' a 0 3').test(output)).to.be.ok
+      expect(reg('foo', ' b 1 3').test(output)).to.be.ok
+      expect(reg('foo', ' c 2 3').test(output)).to.be.ok
+    })
   })
 
   !client && it('should print in color if exists', function() {
-    if (!owner.console) return
-    var prefix = 'foo'
-      , realRed = String.prototype.red
-      , realError = owner.console.error
-      , realConsole = owner.console
-      , result
+    init(function(){
+      var realRed = String.prototype.red
 
-    delete owner.console
-    owner.console = { error: function(){ 
-      result = to.arr(arguments).shift(); 
-      realError.apply && realError.apply(realConsole, arguments) } }
-    String.prototype.red = 'baz'
+      String.prototype.red = 'red_string'
 
-    expect(err(prefix)('bar')).to.equal('bar')
-    expect(result).to.equal('baz')
+      expect(err('foo')('bar')).to.equal('bar')
+      expect(output).to.be.eql('red_string bar\n')
 
-    owner.console = realConsole
-    String.prototype.red = realRed
+      String.prototype.red = realRed
+    })
   })
 
 })
+
+function init(fn){
+  output = '' 
+  /* istanbul ignore next */
+  if (!owner.console) return
+  realConsole = owner.console
+  realLog     = owner.console.error
+  owner.console.error = function(){
+    realLog.apply(realConsole, arguments)
+    output += to.arr(arguments).join(' ') + '\n'
+  }
+  try { fn() } finally {
+    owner.console     = realConsole
+    owner.console.error = realLog
+  }
+}
+
+function reg(ns, info) {
+  return new RegExp('\\[err\\]\\['+iso+'\\]\\['+ns+'\\]' + (info || ''), 'mg')
+}
